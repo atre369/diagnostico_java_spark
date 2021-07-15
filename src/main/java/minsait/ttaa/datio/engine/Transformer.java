255aa67d5970d4c5cc3bae46331b266a99f10f4b
Atreyu <moralesatreyu@hotmail.com>
2021-07-15 10:53:14 -0500

    solved

--- src/main/java/minsait/ttaa/datio/engine/Transformer.java
+++ src/main/java/minsait/ttaa/datio/engine/Transformer.java
@@ -1,16 +1,18 @@
 package minsait.ttaa.datio.engine;
 
-import org.apache.spark.sql.Column;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import minsait.ttaa.datio.common.naming.Field;
+import minsait.ttaa.datio.common.naming.PlayerInput;
+import minsait.ttaa.datio.common.naming.PlayerOutput;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
 import org.jetbrains.annotations.NotNull;
 
+
 import static minsait.ttaa.datio.common.Common.*;
 import static minsait.ttaa.datio.common.naming.PlayerInput.*;
 import static minsait.ttaa.datio.common.naming.PlayerOutput.*;
+import static org.apache.hadoop.hdfs.server.namenode.ListPathsServlet.df;
 import static org.apache.spark.sql.functions.*;
 
 public class Transformer extends Writer {
@@ -24,7 +26,12 @@ public class Transformer extends Writer {
 
         df = cleanData(df);
         df = exampleWindowFunction(df);
+        df = playerCatWindowFunction(df);
+        df = potentialVsOverall(df);
+        df = filterPlayerCatPotentialOverall(df);
+
         df = columnSelection(df);
+
 
         // for show 100 records after your transformations and show the Dataset schema
         df.show(100, false);
@@ -32,15 +39,27 @@ public class Transformer extends Writer {
 
         // Uncomment when you want write your final output
         //write(df);
+        writeParquet(df);
+
+
     }
 
     private Dataset<Row> columnSelection(Dataset<Row> df) {
+
         return df.select(
-                shortName.column(),
-                overall.column(),
-                heightCm.column(),
-                teamPosition.column(),
-                catHeightByPosition.column()
+                PlayerInput.shortName.column(),
+                PlayerInput.longName.column(),
+                PlayerInput.age.column(),
+                PlayerInput.heightCm.column(),
+                PlayerInput.weightKg.column(),
+                PlayerInput.nationality.column(),
+                PlayerInput.clubName.column(),
+                PlayerInput.overall.column(),
+                PlayerInput.potential.column(),
+                PlayerInput.teamPosition.column(),
+                PlayerOutput.playerCat.column(),
+                PlayerInput.potentialVsOverall.column(),
+                PlayerOutput.catHeightByPosition.column()
         );
     }
 
@@ -94,6 +113,54 @@ public class Transformer extends Writer {
         df = df.withColumn(catHeightByPosition.getName(), rule);
 
         return df;
+    }
+    
+    private Dataset<Row> playerCatWindowFunction(Dataset<Row> df) {
+        WindowSpec w = Window
+                .partitionBy(nationality.column())
+                .orderBy(overall.column().desc());
+
+        Column rank = rank().over(w);
+
+        Column rule = when(rank.$less(10), "A")
+                .when(rank.$less(20), "B")
+                .when(rank.$less(50), "C")
+                .otherwise("D");
+
+        return df.withColumn(PlayerOutput.playerCat.getName(), rule);
+    }
+
+    private Dataset<Row> potentialVsOverall(Dataset<Row> df) {
+        Column dif = (df.col("overall")
+                .divide(df.col("potential")));
+
+        return df.withColumn(potentialVsOverall.getName(),dif);
+    }
+
+    private Dataset<Row> filterPlayerCatPotentialOverall(Dataset<Row> df) {
+        Column filterA = df.col("player_cat").equalTo("A");
+        Column filterB = df.col("player_cat").equalTo("B");
+
+        Column filterC = df.col("player_cat").equalTo("C")
+                .and(df.col("potential_overall").$greater(1.15));
+
+        Column filterD = df.col("player_cat").equalTo("D")
+                .and(df.col("potential_overall").$greater(1.25));
+
+        Column condition = filterA.or(filterB).or(filterC).or(filterD);
+
+        return df.filter(condition);
+    }
+
+    private static void writeParquet( Dataset<Row> df) {
+        df.select(shortName.column(),
+                longName.column(),
+                age.column(),
+                heightCm.column(),
+                weightKg.column(),
+                nationality.column());
+
+        df.coalesce(1).write().parquet(OUTPUT_PATH);
     }
 
 
